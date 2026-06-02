10aa09ba664569595263832a00f3beae5ff5c0b1
Muhammad Tayyab <165478795+MuhammadTayyab20@users.noreply.github.com>
2026-06-02 16:22:45 +0500

    Add files via upload

---
+++ HTML/product.js
@@ -0,0 +1,57 @@
+const products = [
+    {
+      id: 0,
+      name: "Shoe- 1",
+      price: 29.99,
+      instock: 100,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "image/shoes1.png",
+    },
+    {
+      id: 1,
+      name: "Shoe- 2",
+      price: 24.99,
+      instock: 43,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "image/shoes2.png",
+    },
+    {
+      id: 2,
+      name: "Shoe- 3",
+      price: 19.99,
+      instock: 10,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "image/shoes3.png",
+    },
+    {
+      id: 3,
+      name: "Shoe- 4",
+      price: 25.99,
+      instock: 5,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "image/shoes4.png",
+    },
+    {
+      id: 4,
+      name: "Shoe- 5",
+      price: 29.99,
+      instock: 4,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "image/shoes5.png",
+    },
+    {
+      id: 5,
+      name: "Shoe- 6",
+      price: 39.99,
+      instock: 40,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "image/shoes6.png",
+    },
+  ];
+  
